77ccb25de9860496e10789a0140a82ec9b8ab7dd
Layanga-Rajapakshe <dimutech46@gmail.com>
2025-05-06 14:57:54 +0530

    home title, add task button, add task page created

---
+++ taskbuddy/lib/addTask.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AddTask extends StatefulWidget {
+  const AddTask({super.key});
+
+  @override
+  State<AddTask> createState() => _AddTaskState();
+}
+
+class _AddTaskState extends State<AddTask> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Add Task",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      body: Container(
+        child: const Text(
+          "Add Task",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
