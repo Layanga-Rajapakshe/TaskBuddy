60af86517fdb7ab56f08754d13174c99b4b2c7e0
Layanga-Rajapakshe <dimutech46@gmail.com>
2025-05-06 17:47:44 +0530

    add task page ui finish

--- taskbuddy/lib/addTask.dart
+++ taskbuddy/lib/addTask.dart
@@ -10,25 +10,67 @@ class AddTask extends StatefulWidget {
 class _AddTaskState extends State<AddTask> {
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
           "Add Task",
           style: TextStyle(
-            color: Colors.white,
+            color: Colors.black,
           ),
         ),
         iconTheme: IconThemeData(
-          color: Colors.white,
+          color: Colors.black,
         ),
-        backgroundColor: Colors.black,
+        backgroundColor: Color(0xF7D44C),
       ),
-      body: Container(
-        child: const Text(
-          "Add Task",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 24,
+      body: Center(
+        child: Container(
+          color: Color(0xF7D44C),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                const TextField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Task...",
+                    hintStyle: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.02,
+                ),
+                ElevatedButton(
+                  onPressed: (){
+
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      vertical: screenHeight * 0.02,
+                      horizontal: screenWidth * 0.2,
+                    ),
+                  ),
+                  child:
+                    const Text(
+                      "Add Task",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                )
+              ],
+            ),
           ),
         ),
       ),
